70d714650218d1884e41d14f886b170b43354e22
Hakon <harnengh@gmail.com>
2015-09-10 18:40:24 +0200

    expose seed

--- src/main/java/com/instantviking/tilenol/board/BoardFactory.java
+++ src/main/java/com/instantviking/tilenol/board/BoardFactory.java
@@ -2,6 +2,7 @@ package com.instantviking.tilenol.board;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import com.instantviking.tilenol.tiles.Tile;
@@ -12,18 +13,29 @@ import com.instantviking.tilenol.tiles.Tile;
  */
 public class BoardFactory
 {
+  private long usedSeed;
+  private Optional<Long> userOverriddenSeed = Optional.empty();
+
   public static void main(String... args)
   {
-    Board b = BoardFactory.generateValidBoard(3, 5);
+    Board b = new BoardFactory().generateValidBoard(3, 5);
     System.out.println(b.toString());
+  }
+
+  public BoardFactory withSeed(long provided_seed)
+  {
+    userOverriddenSeed = Optional.of(provided_seed);
+    return this;
   }
 
   /**
    * Generates a valid board, i.e. one where no tiles point a connection at a
    * neighbor without a matching connection.
    */
-  public static Board generateValidBoard(int width, int height)
+  public Board generateValidBoard(int width, int height)
   {
+    Random rand = buildRandom();
+
     Board b = new Board(width, height);
     List<Position> unusedCells = new ArrayList<Position>();
     for (int i = 0; i < width; i++)
@@ -34,7 +46,6 @@ public class BoardFactory
       }
     }
 
-    Random rand = new Random(new Random().nextLong());
     while (!unusedCells.isEmpty())
     {
       int index = rand.nextInt(unusedCells.size());
@@ -43,9 +54,25 @@ public class BoardFactory
       List<Tile> options = b.tileOptionsAt(current.x, current.y);
       Tile nextGeneratedTile = options.get(rand.nextInt(options.size()));
       b.putTile(nextGeneratedTile, current);
-
     }
     return b;
+  }
+
+  private Random buildRandom()
+  {
+    if (userOverriddenSeed.isPresent())
+    {
+      usedSeed = userOverriddenSeed.get();
+    } else
+    {
+      usedSeed = new Random().nextLong();
+    }
+    return new Random(usedSeed);
+  }
+  
+  public long getUsedSeed()
+  {
+    return usedSeed;
   }
 
 }
